530bee798403e87394a32d15ca2be62314fa98f9
Claudio <63803118+Cloudio69@users.noreply.github.com>
2022-06-20 17:04:26 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ExpensesItems from "./components/ExpensesItem/ExpensesItem";
+import { data } from "./components/data/expenseData";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="expenses-app">
+      <h1> You've got money to spend!</h1>
+      <div className="expense-items">
+        {data.map((item) => (
+          <ExpensesItems
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))}
+      </div>
     </div>
   );
 }
